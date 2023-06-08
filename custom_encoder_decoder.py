84dce11e4df8558302f88554bb82d8b223834233
Luca Barco <luca.barco@linksfoundation.com>
2023-06-08 15:27:24 +0200

    add EncDec with auxiliary head training  support

---
+++ custom_encoder_decoder.py
@@ -0,0 +1,48 @@
+from typing import Optional
+from torch import Tensor
+from mmseg.models.segmentors.encoder_decoder import EncoderDecoder
+from mmseg.utils import (ConfigType, OptConfigType, OptMultiConfig,
+                         OptSampleList, SampleList, add_prefix)
+from mmseg.registry import MODELS
+
+@MODELS.register_module()
+class CustomEncoderDecoder(EncoderDecoder):
+    def __init__(self, backbone: ConfigType,
+                 decode_head: ConfigType,
+                 neck: OptConfigType = None,
+                 auxiliary_head: OptConfigType = None,
+                 train_cfg: OptConfigType = None,
+                 test_cfg: OptConfigType = None,
+                 data_preprocessor: OptConfigType = None,
+                 pretrained: Optional[str] = None,
+                 init_cfg: OptMultiConfig = None):
+        super().__init__(backbone,
+                 decode_head,
+                 neck,
+                 auxiliary_head,
+                 train_cfg,
+                 test_cfg,
+                 data_preprocessor,
+                 pretrained,
+                 init_cfg)
+
+    def _forward(self,
+                 inputs: Tensor,
+                 data_samples: OptSampleList = None) -> Tensor:
+        """Network forward process.
+
+        Args:
+            inputs (Tensor): Inputs with shape (N, C, H, W).
+            data_samples (List[:obj:`SegDataSample`]): The seg
+                data samples. It usually includes information such
+                as `metainfo` and `gt_sem_seg`.
+
+        Returns:
+            Tensor: Forward output of model without any post-processes.
+        """
+        x = self.extract_feat(inputs)
+        x_h1 = self.decode_head.forward(x)
+        if self.auxiliary_head is not None:
+            x_h2 = self.auxiliary_head.forward(x)
+            return x_h1, x_h2
+        return x_h1
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from mmengine import Config
 from mmengine.registry import init_default_scope
 from mmseg.registry import MODELS
 from mmseg.models import EncoderDecoder
+from custom_encoder_decoder import CustomEncoderDecoder
 import torch
 from torch import nn
 
@@ -28,18 +29,44 @@ config_file = "segformer_cfg.py"
 
 # test a single image and show the results
 img = "demo.png"  # or img = mmcv.imread(img), which will only load it once
-override = dict(model=dict(backbone=dict(in_channels=12)))
+
+# override = dict(model=dict(backbone=dict(in_channels=12)))
+
+
+
+override = dict(model=dict(type='CustomEncoderDecoder',
+                           backbone=dict(in_channels=12),
+                           decode_head=dict(type='SegformerHead', 
+                                            num_classes= 1, #TODO
+                                            loss_decode= dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight= 1.0)), #use_sigmoid = True makes it a BCE loss
+                           auxiliary_head=dict(type='SegformerHead', 
+                                            in_channels= [32, 64, 160, 256],
+                                            in_index= [0, 1, 2, 3],
+                                            channels= 256,
+                                            dropout_ratio= 0.1,
+                                            num_classes= 19,  #TODO
+                                            norm_cfg= dict(type= 'SyncBN', requires_grad= True),
+                                            align_corners= False,
+                                            loss_decode= dict(type='CrossEntropyLoss', use_sigmoid=False, loss_weight= 1.0)) # class weights..?
+                        )
+                )
+
 model = build_model(config_file, device="cpu", override=override)
 model.eval()
 # forward stuff
 x = torch.randn(1, 12, 512, 512)
 y = torch.randint(0, 19, (1, 128, 128))
-criterion = nn.CrossEntropyLoss()
+y_binary = torch.randint(0, 2, (1, 128, 128)).unsqueeze(1).float()
+print(y.shape)
+print(y_binary.shape)
+criterion_decode= nn.BCEWithLogitsLoss()
+criterion_auxiliary = nn.CrossEntropyLoss()
 optim = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0001)
 
-out = model(x)
-loss = criterion(out, y)
-loss.backward()
+# out = model(x, mode="loss")
+decode_out, auxiliary_out = model(x)
+loss_decode = criterion_decode(decode_out, y_binary)
+loss_auxiliary = criterion_auxiliary(auxiliary_out, y)
+(loss_decode + loss_auxiliary).backward()
 optim.step()
 
-print(out)
