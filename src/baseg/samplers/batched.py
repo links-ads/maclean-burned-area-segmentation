5f4621c3bf1eae740c500170dd0536bb6e1b3367
Edoardo Arnaudo <edoardo.arnaudo@linksfoundation.com>
2023-06-09 15:33:15 +0200

    Add working datamodule

--- src/baseg/samplers/batched.py
+++ src/baseg/samplers/batched.py
@@ -11,33 +11,18 @@ class RandomTiledBatchSampler(TiledSampler):
         dataset: Dataset,
         tile_size: int,
         batch_size: int,
-        drop_last: bool = False,
-        stride: int = None,
-        apply_pad: bool = False,
         length: int = None,
     ):
         if not isinstance(batch_size, int) or isinstance(batch_size, bool) or batch_size <= 0:
             raise ValueError(
                 "batch_size should be a positive integer value, " "but got batch_size={}".format(batch_size)
             )
-        if not isinstance(drop_last, bool):
-            raise ValueError("drop_last should be a boolean value, but got " "drop_last={}".format(drop_last))
-        super().__init__(dataset, tile_size, stride, apply_pad)
+        super().__init__(dataset, tile_size, length=length)
         self.batch_size = batch_size
-        self.drop_last = drop_last
-        self.length = length or self.estimate_length()
-        self.length = self.length // self.batch_size - 1 if self.drop_last else self.length // self.batch_size
-        self.indices = np.random.choice(len(self.dataset), self.length, replace=True)
+        self.indices = np.random.choice(len(self.dataset), len(self), replace=True)
 
     def __len__(self):
-        return self.length
-
-    def estimate_length(self) -> int:
-        # compute the number of tiles in each image and sum them up
-        return sum(
-            (width - self.tile_size + 1) * (height - self.tile_size + 1) // (self.stride**2)
-            for width, height in self.shapes
-        )
+        return super().__len__() // self.batch_size
 
     def __iter__(self):
         # sample a random image, then sample a random tile from that image
