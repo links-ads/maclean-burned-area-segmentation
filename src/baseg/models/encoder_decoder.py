afa64f28fffecdebb70470d1d55e33a2cb246196
Luca Barco <luca.barco@linksfoundation.com>
2023-06-12 18:48:31 +0200

    fix aux flag

--- src/baseg/models/encoder_decoder.py
+++ src/baseg/models/encoder_decoder.py
@@ -4,7 +4,6 @@ from mmseg.utils import OptSampleList
 from torch import Tensor
 from torch.nn import functional as F
 
-
 @MODELS.register_module()
 class CustomEncoderDecoder(EncoderDecoder):
     def _forward(self, inputs: Tensor, data_samples: OptSampleList = None) -> Tensor:
@@ -22,7 +21,7 @@ class CustomEncoderDecoder(EncoderDecoder):
         x = self.extract_feat(inputs)
         x_h1 = self.decode_head(x)
         x_h1 = F.interpolate(x_h1, size=inputs.shape[2:], mode="bilinear", align_corners=True)
-        if self.auxiliary_head is not None:
+        if self.with_auxiliary_head:
             x_h2 = self.auxiliary_head(x)
             x_h2 = F.interpolate(x_h2, size=inputs.shape[2:], mode="bilinear", align_corners=True)
             return x_h1, x_h2
